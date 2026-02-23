77499bc14542217795d0ec2d97294d2127cedbcf
Johannes Förstner <johannes@famfoerstner.de>
2026-02-23 21:31:57 +0100

    Generate all amulets with the help of a new GasPy printout!

--- generate.bat
+++ generate.bat
@@ -2,6 +2,14 @@
 set bits=%~dp0.
 
 pushd "%GasPy%"
+venv\Scripts\python -m printouts.multiclass_amulets
+if %errorlevel% neq 0 pause
+popd
+
+copy "%GasPy%\output\multiclass-amulets.csv" "%bits%\gaspy\amulets.csv"
+if %errorlevel% neq 0 pause
+
+pushd "%GasPy%"
 venv\Scripts\python -m jinja gaspy world\contentdb\templates\multiclass-amulets --for-each gaspy\amulets.csv --bits "%bits%"
 if %errorlevel% neq 0 pause
 popd
